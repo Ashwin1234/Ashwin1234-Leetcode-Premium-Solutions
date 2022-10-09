b380b5f137c223fcc2203a0e881745ba27ffb569
Ashwin Bhaskar Srivatsa <ashwinsrivatsab@gmail.com>
2022-10-09 12:21:44 -0700

    Time: 839 ms (25.00%), Space: 14.1 MB (62.50%) - LeetHub

---
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
+        task = []
+        output = []
+        max_task = -1
+        task.append((logs[0][0], logs[0][1]))
+        for i in range(1, len(logs)):
+            task.append((logs[i][0], logs[i][1] - logs[i-1][1]))
+        for ele in task:
+            if ele[1] > max_task:
+                max_task = ele[1]
+        for ele in task:
+            if ele[1] == max_task:
+                output.append((ele[0], ele[1]))
+        output = sorted(output, key = lambda x: x[0])
+        return output[0][0]
+        return 1
