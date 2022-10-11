282fbb9dc6947e118d7e2fdba7baed8fe2edbbc1
Ashwin Bhaskar Srivatsa <ashwinsrivatsab@gmail.com>
2022-10-11 00:28:42 -0700

    Time: 452 ms (73.03%), Space: 14.5 MB (41.08%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = set()
+        queue = []
+        count = 0
+        for i in range(0,n+1):
+            if int(math.sqrt(i)) * int(math.sqrt(i)) == i:
+                squares.add(i)
+        squares.pop()
+        print(squares)
+        if n in squares:
+            return 1
+        for ele in squares:
+            if n-ele in squares:
+                return count+2
+            queue.append(n-ele)
+        count = count + 1
+
+        while(len(queue) > 0):
+            print(queue)
+            for i in range(0, len(queue)):
+                ele1 = queue.pop(0)
+                for num in squares:
+                    if ele1-num > 0:
+                        if ele1-num in squares:
+                            count = count + 1
+                            return count + 1
+                        queue.append(ele1-num)
+            count = count + 1
+        return 0
