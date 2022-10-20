dad628e1d843a540f4955c2a9de2af8a338acda8
Ashwin Bhaskar Srivatsa <ashwinsrivatsab@gmail.com>
2022-10-19 22:55:09 -0700

    Time: 3157 ms (38.07%), Space: 52.8 MB (59.56%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -5,7 +5,6 @@ class Solution:
         count = 0
         for i in range(0, len(intervals) - 1):
             second = intervals[i+1]
-            print(first, second)
             if second[0] < first[1] or second[0] == first[0]:
                 if second[1] <= first[1]:
                     count = count + 1
